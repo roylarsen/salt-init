1730bdca46a00ad91b1213ba652ae54b12da555d
Roy <roy.larsen@hbgusa.com>
2016-04-22 16:06:59 -0400

    Repo init

---
+++ salt-init.py
@@ -0,0 +1,40 @@
+import sys, getopt, os
+
+def create_formula(name):
+  if not os.path.exists(name):
+    print "Creating " + name + "..."
+    os.makedirs(name)
+    print "Creating " + name + "/files/..."
+    os.makedirs(name + "/files")
+    print "Creating " + name + "/templates/..."
+    os.makedirs(name + "/templates")
+    print "Creating " + name + "/init.sls..."
+    with open(name + "/init.sls", "a+") as f:
+      f.write('{% from "' + name + '/map.jinja" import ' + name + ' with context %}')
+      f.close()
+    print "Creating " + name + "/map.jinja..."
+    with open(name + "/map.jinja", "a+") as f:
+      map_content = "{% set " + """{NAME} = salt['grains.filter_by']({{
+    'default': {{}}
+}},
+merge=salt['pillar.get']('{NAME}:lookup'))
+""".format(NAME = name) + "%}"
+      f.write(map_content)
+      f.close()
+
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "hf:")
+  except getopt.GetoptError:
+    print 'salt-init.py -f <Formula Name>'
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'salt-init.py -f <Formula Name>'
+      sys.exit()
+    elif opt in ("-f"):
+      create_formula(arg)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
