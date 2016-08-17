2c1be924bde8415ecbcb24ed11c756725783fe04
Roy Larsen <roy.larsen@hbgusa.com>
2016-08-17 13:31:01 -0400

    Added setup.py. Starting to re-architect to be a proper package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='salt-init',
+  version='0.1',
+  packages=find_packages(),
+  install_requires=[
+    'Click',
+  ],
+  entry_points='''
+    [console_scripts]
+    salt-init=salt_init.cli:run
+  ''',
+)
